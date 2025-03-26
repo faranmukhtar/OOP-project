9c5c847f59e55c02bcc25068e167d9775325c370
hmad213 <142343785+hmad213@users.noreply.github.com>
2025-03-26 14:03:08 +0500

    Add classes.cpp and render.cpp

---
+++ src/classes.cpp
@@ -0,0 +1,68 @@
+// #include <iostream>
+// using namespace std;
+
+// // USE VECTORS FOR COORDINATES LATER
+
+// //Unsure if this is needed
+// class Projectile{
+
+// };
+
+// //Might have to change attributes for specialized weapons
+// class Weapon{
+//     private:
+//         int ammo;
+//         int maxAmmo;
+//         int durability;
+//     public:
+//         Weapon(int);
+
+//         //Returns probably a projectile object. Check later
+//         void shoot();
+
+//         //Unsure whether this is needed
+//         void reload();
+
+//         int getAmmo(){return ammo;}
+// };
+
+// class Player{
+//     private:
+//         int x;
+//         int y;
+//         int health;
+//         Weapon* weapon = nullptr;
+//     public:
+//         Player(int, int, int, Weapon*);
+
+//         void assignWeapon(Weapon*);
+
+//         void move(int, int);
+
+//         int getX(){return x;}
+//         int getY(){return y;}
+// };
+
+// class Enemy : public Player{
+
+// };
+
+// class User : public Player{
+
+// };
+
+// //Setup different types of obstacles later
+// class Obstacle{
+//     private:
+//         int x;
+//         int y;
+//     public:
+//         Obstacle(int, int);
+
+//         int getX(){return x;}
+//         int getY(){return y;}
+// };
+
+// int main(void){
+
+// }
