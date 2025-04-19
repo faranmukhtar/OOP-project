364e1f49a6533854837fd79ac16367fa634a21e3
Alyan246 <179561337+Alyan246@users.noreply.github.com>
2025-04-19 19:54:15 +0500

    added more definition and logic to enemy classes: Bomber and Gunner

--- include/player.h
+++ include/player.h
@@ -8,10 +8,12 @@ class Player{
     protected:
         Rectangle hitbox;
         int health;
+        bool alive;
+    
     public:
         Player(double x, double y, double width, double height, int health);
-        virtual void move(int, int);
-        virtual void useWeapon(int, int);
+        virtual void move(int x, int y);
+        virtual void useWeapon(int x, int y);
         Rectangle getHitbox();
         bool isAlive();
         int getHealth();
@@ -19,24 +21,47 @@ class Player{
 };
 
 class Enemy : public Player{
+    protected:
+        float timer;
     public:
-        Enemy(double x, double y, double width, double height, int health) : Player(x, y, width, height, health) {}
+        Enemy(double x, double y, double width, double height, int health) :Player(x, y, width, height, health),timer(0){}
+        virtual void Update() = 0;
+        virtual void takeDamage() = 0;
 };
 
-class Bomber : public Enemy{   //shoots bomb from directly above the user
+class Bomber : public Enemy{  //Drops a bomb from directly above the player
+    private:
+        bool hasDroppedBomb;
+        const float bombInterval = 1.5f;
+        int moveSpeed;
+    
     public:
-        Bomber(double x, double y, double width, double height, int health) : Enemy(x, y, width, height, health){}
+        Bomber(double x, double y) : Enemy(x, y, 40, 40, 1), hasDroppedBomb(false){}
+        void move(int x, int y)override;
+        void useWeapon(int x, int y)override;
+        void takeDamage()override;
+        void draw() override;
 };
 
-class Gunner : public Enemy{   //floating enemy wielding a gun
+class Gunner : public Enemy{  //Shoots the player with its gun (using projectile logic)
+    private:
+        int moveDirection;
+        const float moveInterval = 1.5f;
+        const float shootInterval = 1.0f;
+        float shootTimer;
+    
     public:
-        Gunner(double x, double y, double width, double height, int health) : Enemy(x, y, width, height, health){}
+        Gunner(double x, double y) : Enemy(x, y, 40, 60, 3), moveDirection(1), shootTimer(0){}
+        void useWeapon(int x, int y)override;
+        void draw() override;
+        void takeDamage()override;
+        void move(int x, int y)override;
 };
 
 class User : public Player{
     public:
-        User() : Player(50, GROUND_Y - 100, 50, 100, 100){}
+        User() : Player(50, GROUND_Y - 100, 50, 100, 100) {}
         //Might add more movement mechanics like dash, slide or double jump
-        void draw();
+        void draw() override;
         void jump();
 };
--- src/player.cpp
+++ src/player.cpp
@@ -7,19 +7,15 @@ Player::Player(double x, double y, double width, double height, int health){
     this->hitbox.y = y;
     this->hitbox.height = height;
     this->hitbox.width = width;
-};
-
-void Player::move(int x, int y){
-
-};
-
-//Recheck later
-void Player::useWeapon(int x, int y){
-
+    this->alive = true;
 }
 
+void Player::move(int x, int y){}
+
+void Player::useWeapon(int x, int y){}
+
 bool Player::isAlive(){
-    return health == 0;
+    return alive;
 }
 
 int Player::getHealth(){
@@ -30,10 +26,72 @@ Rectangle Player::getHitbox(){
     return hitbox;
 }
 
+void Bomber::move(int x, int y){
+    if (hasDroppedBomb) {
+        hitbox.y -= moveSpeed; 
+    }
+
+    if (hitbox.y + hitbox.height < 0){  //dies when out of screen
+        alive = false;
+    }
+
+}
+
+void Bomber::useWeapon(int x, int y){
+    if (!alive) return;
+    timer += GetFrameTime();
+
+    if(timer >= bombInterval && !hasDroppedBomb){
+        hasDroppedBomb = true;
+        //return projectile object
+    }
+
+}
+
+void Bomber::draw(){
+    if(alive){
+        DrawRectangleRec(hitbox, GREEN);
+    }
+}
+
+void Bomber::takeDamage(){
+    if(hasDroppedBomb) alive = false;
+}
+
+void Gunner::move(int x, int y){   //moves under defined intervals
+    timer += GetFrameTime();   
+    if (timer >= moveInterval){
+        moveDirection*= -1;
+        timer = 0;
+    }
+    hitbox.x += moveDirection * 1.5f;
+}
+
+void Gunner::useWeapon(int x, int y){  
+    if(!alive) return; 
+
+    shootTimer += GetFrameTime();  //shoots under defined intervals
+    if(shootTimer >= shootInterval){
+        shootTimer = 0;
+        //return projectile object
+    }
+}
+
+void Gunner::draw(){
+    if(alive){
+        DrawRectangleRec(hitbox, BLUE);
+    }
+}
+
+void Gunner::takeDamage(){
+    health--;
+    if (health <= 0) {
+        alive = false;
+    }
+}
+
 void User::draw(){
     DrawRectangleRec(hitbox, BLUE);
 }
 
-void User::jump(){
-
-}
+void User::jump(){}
