2a9357c9e92b72154577d616941cecfea6680524
hmad213 <142343785+hmad213@users.noreply.github.com>
2025-04-19 12:24:57 +0500

    Remove Weapon class

--- include/player.h
+++ include/player.h
@@ -1,7 +1,6 @@
 #pragma once
 #include "raylib.h"
 #include "resource_dir.h"
-#include "weapon.h"
 #include "constants.h"
 using namespace std;
 
@@ -9,10 +8,8 @@ class Player{
     protected:
         Rectangle hitbox;
         int health;
-        Weapon* weapon = nullptr;
     public:
-        Player(double x, double y, double width, double height, int health, Weapon* weapon);
-        virtual void assignWeapon(Weapon*);
+        Player(double x, double y, double width, double height, int health);
         virtual void move(int, int);
         virtual void useWeapon(int, int);
         Rectangle getHitbox();
@@ -23,12 +20,12 @@ class Player{
 
 class Enemy : public Player{
     public:
-        Enemy(double x, double y, double width, double height, int health, Weapon* weapon) : Player(x, y, width, height, health, weapon) {}
+        Enemy(double x, double y, double width, double height, int health) : Player(x, y, width, height, health) {}
 };
 
 class User : public Player{
     public:
-        User() : Player(50, GROUND_Y - 100, 50, 100, 100, nullptr){}
+        User() : Player(50, GROUND_Y - 100, 50, 100, 100){}
         //Might add more movement mechanics like dash, slide or double jump
         void draw();
         void jump();
--- src/player.cpp
+++ src/player.cpp
@@ -1,16 +1,11 @@
 #include "player.h"
 
-Player::Player(double x, double y, double width, double height, int health, Weapon* weapon){
+Player::Player(double x, double y, double width, double height, int health){
     this->health = health;
-    this->weapon = weapon;
     this->hitbox.x = x;
     this->hitbox.y = y;
     this->hitbox.height = height;
     this->hitbox.width = width;
-};
-
-void Player::assignWeapon(Weapon* w){
-    weapon = w;
 };
 
 void Player::move(int x, int y){
